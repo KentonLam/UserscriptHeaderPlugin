ecdaf8c360bc40594659ca92a35e721de7579535
Kenton Lam <kenton_lam@outlook.com>
2018-08-25 19:59:08 +1000

    Regex v1.

--- UserscriptHeaderPlugin.js
+++ UserscriptHeaderPlugin.js
@@ -1,4 +1,9 @@
+const fs = require('fs');
 
+
+// [^\S\r\n] matches whitespace which is not a line break.
+/** Should match a userscript header block. */
+const userscriptHeaderRegex = /^\/\/[^\S\r\n]*==userscript==[^\S\r\n]*\n(\/\/[^\r\n]*\n)+\/\/[^\S\r\n]*==\/userscript==/i;
 
 class UserscriptHeaderPlugin {
     constructor(options) {
@@ -6,6 +11,7 @@ class UserscriptHeaderPlugin {
             throw new Error("Usesrcipt 'filename' not specified.");
         }
         this.filename = options.filename;
+        this.headers = {};
 
         if (typeof this.filename === 'string') {
             this.testFileName = (s) => s.indexOf(this.filename) !== -1;
@@ -17,10 +23,14 @@ class UserscriptHeaderPlugin {
     apply(compiler) {
         const filename = this.filename;
 
-
         compiler.hooks.compilation.tap("UserscriptHeaderPlugin", compilation => {
-            compilation.hooks.buildModule.tap("UserscriptHeaderPlugin", modules => {
-                console.log(module.filename);
+            compilation.hooks.buildModule.tap("UserscriptHeaderPlugin", (module) => {
+                if (this.testFileName(module.resource)) {
+                    console.log("Matched file: "+ module.resource);
+                    let content = fs.readFileSync(module.resource, {encoding: 'utf-8'});
+                    console.log(content);
+                    console.log(userscriptHeaderRegex.exec(content));
+                }
             });
 
             compilation.hooks.optimizeChunkAssets.tap("UserscriptHeaderPlugin", chunks => {
@@ -29,9 +39,7 @@ class UserscriptHeaderPlugin {
                         continue;
 
                     for (const filename of chunk.files) {
-                        if (this.testFileName(filename)) {
-
-                        }
+                            console.log(compilation.assets[filename]);
                     }
                 }
             })
